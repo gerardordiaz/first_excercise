74ea4c78e040e74b2bc619ee636c8dfbc27d5b77
gerardordiaz <jettyrats@gmail.com>
2026-01-06 11:17:52 -0600

    condicionales

--- first_exercise.py
+++ first_exercise.py
@@ -1,2 +1,8 @@
 import os
-print("Hola, terricolas!")
+print(f"Hola, terricolas! {os.getcwd()}")
+
+x = 10 
+if x % 2 == 0:
+    print(f"{x} is an even number")
+else:
+    print(f"{x} is an odd number")
